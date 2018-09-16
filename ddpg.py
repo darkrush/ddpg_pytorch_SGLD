6a338b4fbca0a08a2c0963fc14befbb23e5a52ec
darkrush <kidfatenight@gmail.com>
2018-09-16 20:08:24 +0800

    ns

---
+++ ddpg.py
@@ -0,0 +1,11 @@
+class DDPG(object):
+     def __init__(self):
+         pass
+     def store_transition(self,s_t,,a_t r_t, s_t1, done_t):
+         pass
+     def update(self):
+         pass
+     def get_action(self,s_t):
+         pass
+     def reset(self, obs):
+         pass
